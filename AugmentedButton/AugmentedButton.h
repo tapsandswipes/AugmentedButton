92545768cc25f63d6bcf5ca1e232bf8f5b30b062
Antonio Cabezuelo Vivo <antonio.cabezuelo@latenitesoft.com>
2016-12-26 10:03:15 +0100

    Add Xcode project
    Signed-off-by: Antonio Cabezuelo Vivo <antonio.cabezuelo@latenitesoft.com>

---
+++ AugmentedButton/AugmentedButton.h
@@ -0,0 +1,19 @@
+//
+//  AugmentedButton.h
+//  AugmentedButton
+//
+//  Created by Antonio Cabezuelo Vivo on 25/12/16.
+//  Copyright © 2016 Antonio Cabezuelo Vivo. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for AugmentedButton.
+FOUNDATION_EXPORT double AugmentedButtonVersionNumber;
+
+//! Project version string for AugmentedButton.
+FOUNDATION_EXPORT const unsigned char AugmentedButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AugmentedButton/PublicHeader.h>
+
+
