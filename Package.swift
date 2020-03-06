b3752d4a0f36d07d10a8c39c62660f140facd2ce
Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>
2020-03-06 19:52:02 +0100

    Improve swift package description
    Signed-off-by: Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "AugmentedButton"
+    name: "AugmentedButton",
+    platforms: [
+        .iOS(.v9),
+	],
+	products: [
+		.library(name: "AugmentedButton", targets: ["AugmentedButton"]),
+	],
+	targets: [
+		.target(name: "AugmentedButton", path: "Sources"),
+		
+		.testTarget(name: "AugmentedButtonTests", dependencies: ["AugmentedButton"])
+	]
 )
